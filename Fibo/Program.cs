3ee58948f36b5ab9393b8b85f9486dde5e14983f
dennisStamer <44165732+dennisStamer@users.noreply.github.com>
2020-01-03 18:04:17 +0100

    Added project files

---
+++ Fibo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibo
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fibofolge von 5: " + Fiborecursive(5));
+            Console.ReadKey(true);
+        }
+
+        private static long Fiborecursive(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+            {
+                return Fiborecursive(n - 1) + Fiborecursive(n - 2);
+            }
+
+        }
+    }
+}

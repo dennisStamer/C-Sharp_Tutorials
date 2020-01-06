fbdbf18b51d101b45f40c94fecf3dcde161985da
dstamer <dennis.stamer@merifond.de>
2020-01-06 16:59:21 +0100

    Corrected Fibo printing

--- Fibo/Program.cs
+++ Fibo/Program.cs
@@ -7,21 +7,18 @@ namespace Fibo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Fibofolge von 5: " );
-            Fiborecursive(5);
+            Console.WriteLine("Fibofolge von 5: " + Fiborecursive(7));
             Console.ReadKey(true);
         }
 
         private static long Fiborecursive(int n)
         {
-            int a = 0;
             if (n == 0)
                 return 0;
             else if (n == 1)
                 return 1;
             else
             {
-                Console.WriteLine(n);
                 return Fiborecursive(n - 1) + Fiborecursive(n - 2);
             }
 

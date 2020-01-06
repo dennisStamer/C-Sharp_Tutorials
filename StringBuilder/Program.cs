90309d25cca140ebb4c01d4dcfd0c54255da023b
dennisStamer <44165732+dennisStamer@users.noreply.github.com>
2020-01-06 11:55:29 +0100

    Add files via upload

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringBuilderTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowOutput();
+            Console.ReadKey(true);
+        }
+
+        private static void ShowOutput()
+        {
+            string output = "*";
+
+            var sb = new System.Text.StringBuilder();
+            List<string> list = new List<string>();
+
+            for (int i = 0; i <= 6; i++)
+            {
+                Console.WriteLine(sb.Append(output) + "\n");
+            }
+        }
+    }
+}

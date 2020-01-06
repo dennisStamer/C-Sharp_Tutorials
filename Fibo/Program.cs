c8bfbb9ce1d5157b8fe391e92a4712391a2deb29
dennisStamer <44165732+dennisStamer@users.noreply.github.com>
2020-01-06 16:37:13 +0100

    Add files via upload

---
+++ Fibo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibo
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fibofolge von 5: " );
+            Fiborecursive(5);
+            Console.ReadKey(true);
+        }
+
+        private static long Fiborecursive(int n)
+        {
+            int a = 0;
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+            {
+                Console.WriteLine(n);
+                return Fiborecursive(n - 1) + Fiborecursive(n - 2);
+            }
+
+        }
+    }
+}

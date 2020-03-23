caeae00a89be83b923ba95db0521bdb17d716ee1
dstamer <dennis.stamer@merifond.de>
2020-03-23 15:18:48 +0100

    added c# exercises

---
+++ Chapter 2 - Program 4/Chapter 2 - Program 4/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Chapter_2___Program_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void changeText_Click(object sender, EventArgs e)
+        {
+            if (enableCheckbox.Checked)
+            {
+                if (labelToChange.Text.Equals("Right"))
+                {
+                    labelToChange.Text = "Left";
+                    labelToChange.TextAlign = ContentAlignment.MiddleLeft;
+                }
+                else
+                {
+                    labelToChange.Text = "Right";
+                    labelToChange.TextAlign = ContentAlignment.MiddleRight;
+                }
+            }
+            else
+            {
+                labelToChange.Text = "Text changing is disabled";
+                labelToChange.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+    }
+}
---
+++ FlashyThing/FlashyThing/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FlashyThing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startFlashButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startFlashButton
+            // 
+            this.startFlashButton.Location = new System.Drawing.Point(100, 59);
+            this.startFlashButton.Name = "startFlashButton";
+            this.startFlashButton.Size = new System.Drawing.Size(116, 117);
+            this.startFlashButton.TabIndex = 0;
+            this.startFlashButton.Text = "Go!";
+            this.startFlashButton.UseVisualStyleBackColor = true;
+            this.startFlashButton.Click += new System.EventHandler(this.startFlashButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 276);
+            this.Controls.Add(this.startFlashButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startFlashButton;
+    }
+}
+
